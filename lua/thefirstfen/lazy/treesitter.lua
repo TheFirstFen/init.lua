367b865728f2e131153496f9c4831e4f8a38bf7f
Samuel Berg <sb224sc@student.lnu.se>
2024-05-01 11:26:28 +0200

    added lang:s to treesitter

--- lua/thefirstfen/lazy/treesitter.lua
+++ lua/thefirstfen/lazy/treesitter.lua
@@ -6,7 +6,8 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
+                "jsdoc", "bash", "go", "java", "asm", "c_sharp", "cmake",
+                "cpp", "gomod", "gosum", "gotmpl", "gowork", "matlab",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
